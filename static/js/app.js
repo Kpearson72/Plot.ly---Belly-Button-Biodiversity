b0909e607ba3fd97a2fd0b94fc976b41bd7a40d0
Kpearson72 <kpearson72@gmail.com>
2021-03-05 23:45:17 -0600

    add drop down values to html

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,20 @@
-// Draw a horizontal chart using plotly and the json data
+// Draw charts (bar, bubble) using plotly and the json data
 function buildChart(name){
     d3.json("data/samples.json").then(data => {
         console.log(data)
         let labels = data.samples[0].otu_ids.slice(0,10).map(otuLabel=>`OTU ${otuLabel}`).reverse();
         let sampleValues=data.samples[0].sample_values.slice(0,10).reverse();
         let labelText = data.samples[0].otu_labels.slice(0,10).reverse();
+        // horizontal chart
         let trace1 = {
             y:labels,
             x:sampleValues,
             type: 'bar',
             name: 'Top 10 OTU',
             orientation: 'h',
-            text:labelText
-            
+            text:labelText            
         }
-    
+
         
         Plotly.newPlot('bar', [trace1]);
     
