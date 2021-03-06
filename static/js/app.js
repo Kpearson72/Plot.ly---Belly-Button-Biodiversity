97e17c5e172727427c50e4bba047088e69deaf3c
Kpearson72 <kpearson72@gmail.com>
2021-03-05 22:32:57 -0600

    add bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,9 @@
 // Draw a horizontal chart using plotly and the json data
-
 function buildChart(name){
     d3.json("data/samples.json").then(data => {
         console.log(data)
         let labels = data.samples[0].otu_ids.slice(0,10).map(otuLabel=>`OTU ${otuLabel}`).reverse();
-        let sampleValues = amples[0].sample_values.slice(0,10).reverse();
+        let sampleValues=data.samples[0].sample_values.slice(0,10).reverse();
         let labelText = data.samples[0].otu_labels.slice(0,10).reverse();
         let trace1 = {
             y:labels,
@@ -15,18 +14,10 @@ function buildChart(name){
             text:labelText
             
         }
-
+    
         
         Plotly.newPlot('bar', [trace1]);
-
+    
     });
 }
-// function getdata() {
-//     // Use d3 library to read in samples.json
-//     d3.json("data/samples.json").then(function(json)=>{
-        
-//     }
-// }
-
-
-    
+buildChart(940);
