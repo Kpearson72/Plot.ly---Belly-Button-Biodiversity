e388ef8e72abd03f27f7cc615e20c1fdf30c79f6
Kpearson72 <kpearson72@gmail.com>
2021-03-07 10:26:57 -0600

    add bar plot, image to background of jumbotron, change text

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,47 @@
-// Draw charts (bar, bubble) using plotly and the json data
-function buildChart(name){
-    d3.json("data/samples.json").then(data => {
-        console.log(data)
-        let labels = data.samples[0].otu_ids.slice(0,10).map(otuLabel=>`OTU ${otuLabel}`).reverse();
-        let sampleValues=data.samples[0].sample_values.slice(0,10).reverse();
-        let labelText = data.samples[0].otu_labels.slice(0,10).reverse();
-        // horizontal chart
-        let trace1 = {
-            y:labels,
-            x:sampleValues,
-            type: 'bar',
-            name: 'Top 10 OTU',
-            orientation: 'h',
-            text:labelText            
-        }
+function buildCharts(sample) {
 
-        
-        Plotly.newPlot('bar', [trace1]);
-    
+    // Use `d3.json` to fetch the sample data for the plots
+    d3.json("data/samples.json").then((data) => {
+        let samples = data.samples;
+        let resultsArr = samples.filter(sampleObject => sampleObject.id == sample);
+        let result = resultsArr[0]
+
+        let ids = result.otu_ids;
+        console.log(ids);
+        let labels = result.otu_labels;
+        console.log(labels);
+        let values = result.sample_values;
+        console.log(values);
+        // Draw a horizontal chart using plotly and the json data
+
+        let barData = [
+            {
+                y: ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse(),
+                x: values.slice(0, 10).reverse(),
+                text: labels.slice(0, 10).reverse(),
+                type: "bar",
+                orientation: "h",
+                marker: {
+                    color: 'rgba(222,70,38,0.5)',
+
+                }
+
+            }
+        ];
+
+        let barLayout = {
+            title: "Top 10 OTU",
+            margin: { t: 50, l: 125, b:50},
+            width: 600,
+            height: 400,
+            
+        };
+
+        Plotly.newPlot("bar", barData, barLayout);
+
+
+
+
     });
 }
-buildChart(940);
+buildCharts(940);
